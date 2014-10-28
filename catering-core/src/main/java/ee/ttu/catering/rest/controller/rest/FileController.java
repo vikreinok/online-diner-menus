5c5efc99c17478b175f27065a98c140ce977344c
Ott.Jalakas <Ott123asd@gmail.com>
2014-10-28 19:48:59 +0200

    Added missing files

---
+++ catering-core/src/main/java/ee/ttu/catering/rest/controller/rest/FileController.java
@@ -0,0 +1,20 @@
+package ee.ttu.catering.rest.controller.rest;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import ee.ttu.catering.rest.controller.dto.FileUploadForm;
+
+public interface FileController {
+
+	@ResponseBody
+	@RequestMapping(value="/image/{fileName}", method=RequestMethod.POST)
+	public String createImage(String fileName, FileUploadForm form, BindingResult result);
+
+	@RequestMapping(value="/image/{pictureName}", method=RequestMethod.GET,  produces = MediaType.IMAGE_JPEG_VALUE)
+	public ResponseEntity<byte[]> getImage(String pictureName);
+}
